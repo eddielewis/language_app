6ce4c44c8bbd7de2de53db49cf931998cde52253
Eddie Lewis <eddie.f.lewis@pm.me>
2024-07-13 17:10:52 +0100

    Changed upload path to user_id directory

--- highlights/models.py
+++ highlights/models.py
@@ -8,6 +8,10 @@ class Highlight(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     text = models.CharField(max_length=240)
 
+def user_directory_path(instance, filename):
+    # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
+    return 'user_{0}/{1}'.format(instance.user.id, filename)
+
 class HighlightsSqlite(models.Model):
-    document = models.FileField(upload_to="user_uploads/")
+    document = models.FileField(upload_to=user_directory_path)
     uploaded_at = models.DateTimeField(auto_now_add=True)
